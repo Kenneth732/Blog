59b0861721a4ae07ba75136cd771c58465940f85
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-10-28 15:42:41 +0300

    nav

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -30,42 +30,10 @@ function Navbar() {
                         </ul>
                     </nav>
 
-                    <div class="wrapper">
-
-                        <button class="search-btn" aria-label="search" data-search-toggler>
-                            <ion-icon name="search-outline" aria-hidden="true"></ion-icon>
-
-                            <span class="span">Search</span>
-                        </button>
-
-                        <button class="nav-toggle-btn" aria-label="toggle menu" data-nav-toggler>
-                            <span class="span one"></span>
-                            <span class="span two"></span>
-                            <span class="span three"></span>
-                        </button>
-
-                        <a href="#" class="btn">Join</a>
-
-                    </div>
-
                 </div>
             </header>
 
 
-            <div class="search-bar" data-search-bar>
-
-                <div class="input-wrapper">
-                    <input type="search" name="search" placeholder="Search" class="input-field" />
-
-                    <button class="search-close-btn" aria-label="close search bar" data-search-toggler>
-                        <ion-icon name="close-outline" aria-hidden="true"></ion-icon>
-                    </button>
-
-                </div>
-
-                <p class="search-bar-text">Please enter at least 3 characters</p>
-
-            </div>
 
             <div class="overlay" data-overlay data-search-toggler></div>
 
