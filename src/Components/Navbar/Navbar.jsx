9efc464265a8daaf93ead3cb9ad2f2d448e75377
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-10-28 15:43:04 +0300

    wrapper

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -30,6 +30,24 @@ function Navbar() {
                         </ul>
                     </nav>
 
+                    <div class="wrapper">
+
+                        <button class="search-btn" aria-label="search" data-search-toggler>
+                            <ion-icon name="search-outline" aria-hidden="true"></ion-icon>
+
+                            <span class="span">Search</span>
+                        </button>
+
+                        <button class="nav-toggle-btn" aria-label="toggle menu" data-nav-toggler>
+                            <span class="span one"></span>
+                            <span class="span two"></span>
+                            <span class="span three"></span>
+                        </button>
+
+                        <a href="#" class="btn">Join</a>
+
+                    </div>
+
                 </div>
             </header>
 
