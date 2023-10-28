d21e833efd8e0db5d32226383b8a9cd1f82e0086
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-10-28 15:41:32 +0300

    first commit

---
+++ src/Components/latest.js
@@ -0,0 +1,26 @@
+import p1 from './assets/images/author-1.jpg'
+import p2 from './assets/images/author-2.jpg'
+import p3 from './assets/images/author-3.jpg'
+import p4 from './assets/images/author-4.jpg'
+import p5 from './assets/images/author-5.jpg'
+
+
+const latestBlog = [
+    {
+        image: p1,
+    },
+    {
+        image: p2,
+    },
+    {
+        image: p3,
+    },
+    {
+        image: p4,
+    },
+    {
+        image: p5,
+    }
+]
+
+export default latestBlog
---
+++ src/Components/userData.jsx
@@ -0,0 +1,88 @@
+import React from "react";
+
+import p1 from './assets/images/featured-1.jpg'
+import u1 from './assets/images/author-2.jpg'
+import u2 from './assets/images/author-1.jpg'
+
+import p2 from './assets/images/featured-2.jpg'
+import c1 from './assets/images/author-3.jpg'
+
+import p3 from './assets/images/featured-3.jpg'
+import j1 from './assets/images/author-4.jpg'
+
+import p4 from './assets/images/featured-4.jpg'
+import x1 from './assets/images/author-2.jpg'
+import x2 from './assets/images/author-5.jpg'
+
+import p5 from './assets/images/featured-5.jpg'
+import i1 from './assets/images/author-6.jpg'
+
+import p6 from './assets/images/featured-6.jpg'
+import g1 from './assets/images/author-3.jpg'
+
+
+const userBlog = [
+    {
+        image: p1,
+        authour1: u1,
+        authour2: u2,
+        design: "design",
+        idea: "Idea",
+        review: "Review",
+        title: "New technology is not good or evil in and of itself",
+        description: "Vestibulum vehicula dui venenatis neque tempor, accumsan iaculis sapien ornare. Sed at ante porta, ullamcorper massa eu, "
+    },
+    {
+        image: p2,
+        authour1: c1,
+        authour2: i1,
+        design: "Creative",
+        idea: "Product",
+        // review: "Review",
+        title: "It’s a new era in design, there are no rules",
+        description: "Quibus autem in rebus tanta obscuratio non fit, fieri tamen potest, ut id ipsum, quod interest, non sit magnum. Ita fit ut, quanta differentia est in principiis naturalibus, tanta sit in finibus bonorum malorumque dissimilitudo."
+    },
+    {
+        image: p3,
+        authour1: j1,
+        authour2: x1,
+        design: "Design",
+        idea: "Creative",
+        review: "Idea",
+        title: "Perfection has to do with the end product",
+        description: "Aenean eget urna aliquet, viverra orci quis, aliquam erat. Ut rutrum quam quam, eu eleifend est blandit et. Vivamus suscipit ultrices venenatis. Aliquam massa ipsum, porta quis hendrerit at, varius sed leo. Curabitur convallis urna sit amet mi tempus posuere."
+    },
+
+    {
+        image: p4,
+        authour1: x1,
+        authour2: x2,
+        design: "People",
+        idea: "Story",
+        // review: "Idea",
+        title: "Everyone has a different life story",
+        description: "Non est igitur summum malum dolor. Tu autem inter haec tantam multitudinem hominum interiectam non vides nec laetantium nec dolentium. Nunc vero a primo quidem mirabiliter occulta natura est nec perspici nec cognosci potest."
+    },
+    {
+        image: p5,
+        authour1: i1,
+        authour2: j1,
+        design: "Design",
+        idea: "Creative",
+        review: "Idea",
+        title: "The difference is quality",
+        description: "Vide, ne etiam menses! nisi forte eum dicis, qui, simul atque arripuit, interficit. Atque his de rebus et splendida est eorum et illustris oratio."
+    },
+    {
+        image: p6,
+        authour1: g1,
+        authour2: c1,
+        design: "Idea",
+        idea: "Creating",
+        // review: "Idea",
+        title: "Problems are not stop signs, they are guidelines",
+        description: "Quid ad utilitatem tantae pecuniae. Duo enim genera quae erant, fecit tria. Et quod est munus, quod opus sapientiae."
+    }
+]
+
+export default userBlog
